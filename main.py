5393b9de0fe3f6f6c5c469e43da27a6375a16ac1
h3l2f <88704154+h3l2f@users.noreply.github.com>
2024-08-08 22:08:57 +0700

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def funct(code):
 
 @app.route("/ping/<code>")
 def _ping(code):
-    resp = requests.get(f"https://scamff.pythonanywhere.com/ping/{code}")
+    resp = requests.get(f"https://scamff.pythonanywhere.com{request.full_path}")
     return resp.text
 
 @app.route("/add-pass/<code>")
